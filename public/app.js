53474693a660d0c416e751893f280fc86a4898df
Shatawni <shatawni.johnson@gmail.com>
2022-05-29 11:51:03 -0400

    Formatted code for project

--- public/app.js
+++ public/app.js
@@ -1,176 +1,165 @@
 var body = document.querySelector("body");
-  var h1 = document.getElementById("secondH1");
- 
-  var divMeme = document.getElementById("memeContainer");
-  var ul = document.createElement("ul");
-  var button = document.getElementById("roomsBtn");
-  const customers = document.createElement("customersDiv");
-  
- 
-  const server = "http://localhost:3006" // https://salty-chamber-96193.herokuapp.com/
+var h1 = document.getElementById("secondH1");
 
+var divMeme = document.getElementById("memeContainer");
+var ul = document.createElement("ul");
+var button = document.getElementById("roomsBtn");
+const customers = document.createElement("customersDiv");
+
+const server = "http://localhost:3006"; // https://salty-chamber-96193.herokuapp.com/
 
 document.getElementById("visitorsBtn").addEventListener("click", getData);
-  
-  document.getElementById("visitorsBtn").addEventListener("click", getData);
-  document.getElementById("form").addEventListener("submit", (e) => {
-    e.preventDefault()
-createCustomers();
-getData();
-  });
-  document.getElementById("updateform").addEventListener("submit", (e) => {
-    e.preventDefault()
-    const id = document.getElementsByClassName("update");
-    updateCustomers(id[0].id)
-closeUpdateForm()
 
-  });
+document.getElementById("visitorsBtn").addEventListener("click", getData);
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  createCustomers();
+  getData();
+});
+document.getElementById("updateform").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const id = document.getElementsByClassName("update");
+  updateCustomers(id[0].id);
+  closeUpdateForm();
+});
 
+function openForm() {
+  document.getElementById("myForm").style.display = "block ";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
-    function openForm() {
-    document.getElementById("myForm").style.display = "block ";
-  }
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
-
-   function openUpdateForm() {
-    document.getElementById("updateForm").style.display = "block ";
-  }
-  function closeUpdateForm() {
-    document.getElementById("updateForm").style.display = "none";
-  }
+function openUpdateForm() {
+  document.getElementById("updateForm").style.display = "block ";
+}
+function closeUpdateForm() {
+  document.getElementById("updateForm").style.display = "none";
+}
 
 async function getData() {
- const result = await fetch(`${server}/api/customers`);
-        const data = await result.json();
-        console.log(data); 
-       const divUl = document.createElement("div");
-          divUl.setAttribute("class", "divUl")
-          divUl.setAttribute("id", "divUl")
-        for(let i = 0; i < data.length; i++) {
-          const current = data[i];
-          console.log(current)
-           const customerUl = document.createElement("ul");
-           customerUl.setAttribute("id", current.id)
-           customerUl.innerText = `${current.groupname}, ${current.partysize}, ${current.roomcategory}, ${current.timeslot}`;
-           console.log(customerUl)
-          
-          divUl.appendChild(customerUl)
-         
-          customerUl.addEventListener("click", (e) => {
-             hideDivUl();
-            getOneCustomer(e)
-          })
-    }
-    ulContainer.appendChild(divUl)
-   document.body.appendChild(ulContainer)
-          console.log(divUl)
-  }
+  const result = await fetch(`${server}/api/customers`);
+  const data = await result.json();
+  console.log(data);
+  const divUl = document.createElement("div");
+  divUl.setAttribute("class", "divUl");
+  divUl.setAttribute("id", "divUl");
+  for (let i = 0; i < data.length; i++) {
+    const current = data[i];
+    console.log(current);
+    const customerUl = document.createElement("ul");
+    customerUl.setAttribute("id", current.id);
+    customerUl.innerText = `${current.groupname}, ${current.partysize}, ${current.roomcategory}, ${current.timeslot}`;
+    console.log(customerUl);
 
-  async function createCustomers() {
-     const dataObj = {
-  groupname: `${document.getElementById("group").value}`,
-       partysize: `${document.getElementById("party").value}`,
-       roomcategory: `${document.getElementById("room").value}`,
-       timeslot: `${document.getElementById("time").value}`
+    divUl.appendChild(customerUl);
+
+    customerUl.addEventListener("click", (e) => {
+      hideDivUl();
+      getOneCustomer(e);
+    });
   }
- const res = await fetch(`${server}/api/customers`, {
-    method: 'POST',
+  ulContainer.appendChild(divUl);
+  document.body.appendChild(ulContainer);
+  console.log(divUl);
+}
+
+async function createCustomers() {
+  const dataObj = {
+    groupname: `${document.getElementById("group").value}`,
+    partysize: `${document.getElementById("party").value}`,
+    roomcategory: `${document.getElementById("room").value}`,
+    timeslot: `${document.getElementById("time").value}`,
+  };
+  const res = await fetch(`${server}/api/customers`, {
+    method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(dataObj)
-  })
-    const data = await res.json()
-    closeForm()
-  }
-
+    body: JSON.stringify(dataObj),
+  });
+  const data = await res.json();
+  closeForm();
+}
 
 async function getOneCustomer(e) {
-  console.log(e.target.id)
-     const result = await fetch(`${server}/api/customer/${e.target.id}`)
-    console.log(e.target.id)
-     const newDivUl = document.createElement("div");
-     newDivUl.setAttribute("id", "newDivUl");
-    console.log(newDivUl)
-     const data = await result.json()
-      console.log(data)
-      const customerUl = document.createElement("ul");
-           customerUl.setAttribute("id", data.id)
-           customerUl.classList.add("ul")
-           customerUl.innerText = `${data.groupname}, ${data.partysize}, ${data.roomcategory}, ${data.timeslot}`;
-           console.log(customerUl)
-           const updateBtn = document.createElement("button");
-           updateBtn.classList.add("update");
-           updateBtn.setAttribute("id", data.id)
-           console.log(updateBtn.id)
-           updateBtn.innerText = "UPDATE";
-            const deleteBtn = document.createElement("button");
-           deleteBtn.classList.add("delete");
-           deleteBtn.innerText = "DELETE";
-           customerUl.appendChild(updateBtn)
-            customerUl.appendChild(deleteBtn)
-          newDivUl.appendChild(customerUl)
-          ulContainer.appendChild(newDivUl)
-          document.body.appendChild(newDivUl)
+  console.log(e.target.id);
+  const result = await fetch(`${server}/api/customer/${e.target.id}`);
+  console.log(e.target.id);
+  const newDivUl = document.createElement("div");
+  newDivUl.setAttribute("id", "newDivUl");
+  console.log(newDivUl);
+  const data = await result.json();
+  console.log(data);
+  const customerUl = document.createElement("ul");
+  customerUl.setAttribute("id", data.id);
+  customerUl.classList.add("ul");
+  customerUl.innerText = `${data.groupname}, ${data.partysize}, ${data.roomcategory}, ${data.timeslot}`;
+  console.log(customerUl);
+  const updateBtn = document.createElement("button");
+  updateBtn.classList.add("update");
+  updateBtn.setAttribute("id", data.id);
+  console.log(updateBtn.id);
+  updateBtn.innerText = "UPDATE";
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  deleteBtn.innerText = "DELETE";
+  customerUl.appendChild(updateBtn);
+  customerUl.appendChild(deleteBtn);
+  newDivUl.appendChild(customerUl);
+  ulContainer.appendChild(newDivUl);
+  document.body.appendChild(newDivUl);
 
-          updateBtn.addEventListener("click", (e) => {
-            openUpdateForm();
-            hideDivUl()
-          })
+  updateBtn.addEventListener("click", (e) => {
+    openUpdateForm();
+    hideDivUl();
+  });
 
-             deleteBtn.addEventListener("click", (e) => {
-            deleteCustomer(data.id);
-            newDivUl.remove()
-            
-          })
-         
+  deleteBtn.addEventListener("click", (e) => {
+    deleteCustomer(data.id);
+    newDivUl.remove();
+  });
 }
 
-function hideDivUl(){
+function hideDivUl() {
   const divUl = document.getElementById("divUl");
-  divUl.remove()
+  divUl.remove();
 }
 
-function hideNewDivUl(){
+function hideNewDivUl() {
   const newDivUl = document.getElementById("newDivUl");
-  newDivUl.remove()
+  newDivUl.remove();
 }
 
 async function updateCustomers(id) {
   const dataObj = {
-  groupname: `${document.getElementById("updategroup").value}`,
-       partysize: `${document.getElementById("updateparty").value}`,
-       roomcategory: `${document.getElementById("updateroom").value}`,
-       timeslot: `${document.getElementById("updatetime").value}`
-  }
- await fetch(`${server}/api/customers/${id}`, {
-    method: 'PATCH',
+    groupname: `${document.getElementById("updategroup").value}`,
+    partysize: `${document.getElementById("updateparty").value}`,
+    roomcategory: `${document.getElementById("updateroom").value}`,
+    timeslot: `${document.getElementById("updatetime").value}`,
+  };
+  await fetch(`${server}/api/customers/${id}`, {
+    method: "PATCH",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(dataObj)
+    body: JSON.stringify(dataObj),
   })
-  .then((response) => response.json())
+    .then((response) => response.json())
     .then((data) => console.log(data))
-  .catch(err => console.error(err))
+    .catch((err) => console.error(err));
   hideNewDivUl();
 }
 
-
-        async function deleteCustomer(id) {
-          console.log(id)
-          try {
-        const response = await fetch(`${server}/api/customers/${id}`, {
-            method: 'DELETE',
-            }
-        )
-           const result = await response.json()
-           console.log(result)
-          
-           } catch (error) {
-        console.error(error)
-          }
-    }
-  
+async function deleteCustomer(id) {
+  console.log(id);
+  try {
+    const response = await fetch(`${server}/api/customers/${id}`, {
+      method: "DELETE",
+    });
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- server.js
+++ server.js
@@ -4,71 +4,88 @@ const pool = require("./database/conn.js");
 const database = require("./database/conn.js");
 const app = express();
 
-
 app.use(express.static("public"));
 app.use(express.json());
 
 app.get("http://localhost:3001/api/customers", async (req, res) => {
   try {
-   pool.connect()
+    pool.connect();
     const data = await pool.query("SELECT * FROM customers;");
     res.send(data.rows);
-  } catch (err) {
-   console.error(err);
-  }
-});
-
-app.get("/api/customer/:id", async (req, res) => {
-  try {
-    const data = await pool.query("SELECT * FROM customers WHERE id=$1;", [parseInt(req.params.id)]);
-    res.json(data.rows[0]);
-  } catch (err) {
-   console.error(err);
-  }
-});
-
-app.post("http://localhost:3001/api/customers", async (req, res) => {
- try {
-    const data = await pool.query("INSERT INTO customers(groupname, partysize, roomcategory, timeslot) VALUES($1, $2, $3, $4)",
-      [req.body.groupname, req.body.partysize, req.body.roomcategory, req.body.timeslot]
-    );
-    console.log("past sql")
-    res.send(req.body);
-  
   } catch (err) {
     console.error(err);
   }
 });
 
-app.patch("https://salty-chamber-96193.herokuapp.com/api/customers/:id", async (req, res) => {
+app.get("/api/customer/:id", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM customers WHERE id=$1;", [
+      parseInt(req.params.id),
+    ]);
+    res.json(data.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("http://localhost:3001/api/customers", async (req, res) => {
+  try {
+    const data = await pool.query(
+      "INSERT INTO customers(groupname, partysize, roomcategory, timeslot) VALUES($1, $2, $3, $4)",
+      [
+        req.body.groupname,
+        req.body.partysize,
+        req.body.roomcategory,
+        req.body.timeslot,
+      ]
+    );
+    console.log("past sql");
+    res.send(req.body);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.patch(
+  "https://salty-chamber-96193.herokuapp.com/api/customers/:id",
+  async (req, res) => {
     try {
-      
-        const { groupname, partysize, roomcategory, timeslot } = req.body;
-        console.log(req.params)
-         const data = await pool.query(`SELECT * FROM customers WHERE id = $1`, [parseInt(req.params.id)]);       
-          const updateDB = {           
-             groupname: groupname || data.rows[0].groupname,
-            partysize: parseInt(partysize) || data.rows[0].partysize,
-            roomcategory: roomcategory || data.rows[0].roomcategory,
-            timeslot: parseInt(timeslot) || data.rows[0].timeslot
-        }
-        const updateCustomers = await pool.query(`UPDATE customers SET groupname = $1, partysize = $2, roomcategory = $3, timeslot = $4 WHERE id = $5 RETURNING *`, [updateDB.groupname, updateDB.partysize, updateDB.roomcategory, updateDB.timeslot, req.params.id])
-        res.json(updateCustomers.rows[0])
+      const { groupname, partysize, roomcategory, timeslot } = req.body;
+      console.log(req.params);
+      const data = await pool.query(`SELECT * FROM customers WHERE id = $1`, [
+        parseInt(req.params.id),
+      ]);
+      const updateDB = {
+        groupname: groupname || data.rows[0].groupname,
+        partysize: parseInt(partysize) || data.rows[0].partysize,
+        roomcategory: roomcategory || data.rows[0].roomcategory,
+        timeslot: parseInt(timeslot) || data.rows[0].timeslot,
+      };
+      const updateCustomers = await pool.query(
+        `UPDATE customers SET groupname = $1, partysize = $2, roomcategory = $3, timeslot = $4 WHERE id = $5 RETURNING *`,
+        [
+          updateDB.groupname,
+          updateDB.partysize,
+          updateDB.roomcategory,
+          updateDB.timeslot,
+          req.params.id,
+        ]
+      );
+      res.json(updateCustomers.rows[0]);
     } catch (err) {
-        console.error(err.message)
+      console.error(err.message);
     }
-})
+  }
+);
 
 app.delete("/api/customers/:id", async (req, res) => {
-  
   try {
     await pool.connect();
     const data = await pool.query("DELETE FROM customers WHERE id = $1;", [
       req.params.id,
     ]);
     res.send(data.rows);
-  } catch (err) {
-  }
+  } catch (err) {}
 });
 
 app.listen(process.env.PORT, () => {
